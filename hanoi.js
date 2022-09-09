3f74231c72bddf1e0166ad01120b23f36c4acae6
Danny K <dxkalnz@gmail.com>
2022-09-08 18:16:03 -0600

    Add JS pseudocode and some basic code

--- hanoi.js
+++ hanoi.js
@@ -1,3 +1,27 @@
+//Variable declarations
+
+//UI
+//variables to contain ui elements like restart buttons
+
+//LOGIC
+//Arrays to define the logic contents of the game
+const pegA = [];
+const pegB = [];
+const pegC = [];
+let choice = null; //corresponds to player's choice/ held item
+
+//FUNCTION DEFINITIONS
+
+function welcome() {
+	//shows initial hello screen/alertbox,
+	//contains info for the game
+}
 function init() {
 	//this is to initialize dom elements and draw the main game
+}
+function render() {
+	//this is to draw elements (discs) when someone makes a move
+}
+function reset() {
+	//resets the game
 }
