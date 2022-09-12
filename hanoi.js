a3da3d2754eb7c4e78b5a4f76ecfc797402d8a23
Danny K <dxkalnz@gmail.com>
2022-09-12 06:09:02 -0600

    Complete main game logic

--- hanoi.js
+++ hanoi.js
@@ -52,8 +52,7 @@ function initDiscs() {
 		discCreate.classList.add('discs');
 		discCreate.id = i;
 		pegs[0].appendChild(discCreate);
-		discCreate.style.backgroundColor =
-			'#' + Math.floor(Math.random() * 16777215).toString(16);
+		discCreate.style.backgroundColor = getRandomColor();
 	}
 	console.log(`Discs initiated, left stack has ${stack0}`);
 }
@@ -91,7 +90,7 @@ function pickPeg(num) {
 		holding = allStacks[num].pop();
 		isPicking = false;
 		console.log(
-			`holding is ${holding} and this stack now has${allStacks[num]} and allstackslength is ${allStacks[num].length}`
+			`You are holding ${holding} and this stack now has${allStacks[num]}`
 		);
 	} else {
 		console.log(`Can't pick this`);
@@ -99,21 +98,60 @@ function pickPeg(num) {
 }
 function placePeg(num) {
 	let myStack = allStacks[num];
-	allStacks[num].push(holding);
-	holding = 0;
-	isPicking = true;
+	if (allStacks[num].length == 0) {
+		allStacks[num].push(holding);
+		holding = 0;
+		isPicking = true;
+	} else if (canPlace(num)) {
+		allStacks[num].push(holding);
+		holding = 0;
+		isPicking = true;
+		console.log(`middle thing happened`);
+		// console.log(`result of myFunction is ${myFunction}`);
+	} else {
+		console.log(`can't move here, you're still holding ${holding}`);
+	}
+
 	console.log(
 		`left has ${allStacks[0]}, middle has ${allStacks[1]}, right has ${allStacks[2]}`
 	);
 }
-function iCanHaz(num) {
-	if (this.Stack.contents.length > 0) {
-		return true;
-	} else {
-		return false;
-		alert(`You no can haz!`);
-	}
+function canPlace(num) {
+	console.log(
+		`canPlace ran with ${num} we are returning value of ${
+			holding > allStacks[num]
+		}`
+	);
+	return holding > allStacks[num];
 }
+// function myFunction(num) {
+// 	allStacks[num].some(canPlace);
+// }
+
+function hexToDec(hexString) {
+	return parseInt(hexString, 16);
+}
+//RED GENERATOR
+function getRandomColor() {
+	let min = hexToDec('d14976');
+	let max = hexToDec('fa0556');
+	let rand = null;
+	//console.log(`min is ${min} and max is ${max}`);
+	rand = Math.floor(Math.random() * (max - min) + min).toString(16);
+	// console.log(rand);
+	return '#' + rand;
+	//13393533 min
+	//16385366 max
+}
+
+// function iCanHaz(num) {
+// 	if (this.Stack.contents.length > 0) {
+// 		return true;
+// 	} else {
+// 		return false;
+// 		alert(`You no can haz!`);
+// 	}
+// }
 
 //Functions whose sole purpose is to allow appending of a, b, c etc ID's to DOM created elements
 function abcUTF(lowerCaseLetter) {
