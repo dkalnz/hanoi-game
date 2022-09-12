b8e0a31b13468dba79102d2a5bc36f7e577d1d20
Danny K <dxkalnz@gmail.com>
2022-09-12 16:26:47 -0600

    Fix game logic using every() method and animate held disc

--- hanoi.js
+++ hanoi.js
@@ -10,26 +10,30 @@ init();
 let left = document.querySelector('#a');
 let mid = document.querySelector('#b');
 let right = document.querySelector('#c');
-//let qty = [7, 0, 0];
 
 let stack0 = [];
 let stack1 = [];
 let stack2 = [];
 let allStacks = [stack0, stack1, stack2];
 const pegs = document.querySelectorAll('.pegbox');
+const discs = document.querySelectorAll('.discs');
 let startSize = 0;
 const baseWidthFactor = 23;
 const discWidthFactor = 20;
 let discWidth = 21;
 let discWidthDivisor = 1;
 let pickedPegIdx = 0;
-const statusy = document.querySelector('.status'); //debugging object
+let lastPegIdx = 0;
 const basePlate = document.querySelector('.bases');
+let footy1 = document.querySelector('#foot1');
+let footy2 = document.querySelector('#foot2');
+let footy3 = document.querySelector('#foot3');
 
 initBases();
 let discCreate = null;
 initDiscs();
-render();
+let thisDisc = discCreate.id;
+initRender();
 
 function init() {
 	//INITIALIZE BOARD AND PEGS
@@ -58,18 +62,37 @@ function initDiscs() {
 		pegs[0].appendChild(discCreate);
 		discWidthDivisor = discWidthFactor / startSize;
 		discWidth = discWidthFactor - discWidthDivisor * (i - 1);
-		console.log(
-			`created disc i${i} with factor of ${discWidthFactor} and divisor of ${discWidthDivisor} with width of ${discWidth}`
-		);
 		discCreate.style.width = discWidth + 'vw';
 		discCreate.style.backgroundColor = getRandomColor();
 	}
-	console.log(`Discs initiated, left stack has ${stack0}`);
+	statusReport();
 }
-function render() {
+function statusReport() {
+	let lefty = allStacks[0]; //.slice().reverse();
+	let middley = allStacks[1]; //.slice().reverse();
+	let righty = allStacks[2]; //.slice().reverse();
+	footy1.innerText = lefty.join(' ');
+	footy2.innerText = middley.join(' ');
+	footy3.innerText = righty.join(' ');
+}
+function initRender() {
 	for (i = 0; i < 3; i++) {
 		renderColor(i);
 	}
+	statusReport();
+}
+function render(discId) {
+	let myDisc = document.getElementById(`${discId}`);
+	if (holding > 0) {
+		myDisc.classList.add('held');
+	} else if (holding == 0) {
+		myDisc.classList.remove('held');
+	} else {
+	}
+	for (i = 0; i < 3; i++) {
+		renderColor(i);
+	}
+	statusReport();
 }
 function renderColor(idx) {
 	if (allStacks[idx].length == 0) {
@@ -82,13 +105,12 @@ function renderColor(idx) {
 }
 pegs.forEach((el, num) => {
 	el.addEventListener('click', () => {
-		console.log(`clicked peg ${pegs[num].id}`);
 		if (isPicking) {
 			pickPeg(num);
-			render();
+			render(thisDisc);
 		} else {
 			placePeg(num);
-			render();
+			render(thisDisc);
 		}
 	});
 });
@@ -97,47 +119,44 @@ function pickPeg(num) {
 	let myStackLength = myStack.length;
 	if (myStackLength > 0) {
 		holding = allStacks[num].pop();
+		thisDisc = holding;
 		isPicking = false;
-		console.log(
-			`You are holding ${holding} and this stack now has${allStacks[num]}`
-		);
+		console.log(`You are holding ${thisDisc}`);
 	} else {
 		console.log(`Can't pick this`);
 	}
 }
 function placePeg(num) {
 	let myStack = allStacks[num];
-	if (allStacks[num].length == 0) {
+	if (myStack.length == 0) {
 		allStacks[num].push(holding);
 		holding = 0;
 		isPicking = true;
-	} else if (canPlace(num)) {
+	} else if (allStacks[num].every(canPlace)) {
+		console.log(
+			`allstacks[num] is ${
+				allStacks[num]
+			}, you were just holding ${holding} and canPlace returned ${allStacks[
+				num
+			].some(canPlace)}`
+		);
 		allStacks[num].push(holding);
 		holding = 0;
 		isPicking = true;
 	} else {
 		console.log(`can't move here, you're still holding ${holding}`);
 	}
-	console.log(
-		`left has ${allStacks[0]}, middle has ${allStacks[1]}, right has ${allStacks[2]}`
-	);
 }
-function canPlace(num) {
-	console.log(
-		`canPlace ran with ${num} we are returning value of ${
-			holding > allStacks[num]
-		}`
-	);
-	return holding > allStacks[num];
+function canPlace(thisArraysBiggest) {
+	return holding > thisArraysBiggest;
 }
-// function myFunction(num) {
-// 	allStacks[num].some(canPlace);
-// }
-
+//STYLE
+//	FUNCTIONS
+//		HERE
 function hexToDec(hexString) {
 	return parseInt(hexString, 16);
 }
-//RED GENERATOR
+//COLOR GENERATOR
 function getRandomColor() {
 	let min = hexToDec('d14976');
 	let max = hexToDec('fa0556');
@@ -149,16 +168,6 @@ function getRandomColor() {
 	//13393533 min
 	//16385366 max
 }
-
-// function iCanHaz(num) {
-// 	if (this.Stack.contents.length > 0) {
-// 		return true;
-// 	} else {
-// 		return false;
-// 		alert(`You no can haz!`);
-// 	}
-// }
-
 //Functions whose sole purpose is to allow appending of a, b, c etc ID's to DOM created elements
 function abcUTF(lowerCaseLetter) {
 	return String.fromCharCode(lowerCaseLetter + 97);
